f273d7890fe4b8d156a7f8391aa8b6ed3f1dc258
Manuel Parra-Royón <manuparra@gmail.com>
2023-05-08 22:39:25 +0200

    Update run.py

--- Packages_Manager_and_Containers/pipeline/run.py
+++ Packages_Manager_and_Containers/pipeline/run.py
@@ -2,22 +2,24 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import astropy.io.fits as fits
+from astropy.utils.data import download_file
 from skimage import filters
 
 # Load FITS image
-hdulist = fits.open('data/HCG16_CD_rob2_MS.pbcor.fits')
+image_file = download_file('http://data.astropy.org/tutorials/FITS-images/HorseHead.fits', cache=True )
+hdulist = fits.open(image_file)
 image = hdulist[0].data
 
 # Save FITS image as PNG
-plt.imsave('output/original.png', image, cmap='gray')
+plt.imsave('/output/original.png', image, cmap='gray')
 
 # Apply filters
 gaussian_image = filters.gaussian(image, sigma=2)
 sobel_image = filters.sobel(image)
 
 # Save filtered images as PNGs
-plt.imsave('output/gaussian.png', gaussian_image, cmap='gray')
-plt.imsave('output/sobel.png', sobel_image, cmap='gray')
+plt.imsave('/output/gaussian.png', gaussian_image, cmap='gray')
+plt.imsave('/output/sobel.png', sobel_image, cmap='gray')
 
 # Display original and filtered images
 fig, axs = plt.subplots(2, 2, figsize=(8, 8))
@@ -30,4 +32,4 @@ axs[1, 0].set_title('Sobel')
 axs[1, 1].axis('off')
 
 # Save figure as PNG
-fig.savefig('output/filtered.png')
+fig.savefig('/output/filtered.png')
